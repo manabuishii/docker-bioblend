7b724af10efdbb7aec9dd79ebcaaa74ed425a25c
Manabu ISHII <manabu.ishii.rb@gmail.com>
2016-10-14 06:01:38 +0000

    Add export workflow as json format

---
+++ example/workflow_export_workflow_json.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# coding: utf-8
+"""
+Get workflow as json format
+"""
+import os
+import json
+from bioblend import galaxy
+
+
+galaxy_url = os.getenv("GALAXY_URL", "http://dockergalaxy/")
+api_key=os.getenv("GALAXY_API_KEY", "admin")
+workflow_id=os.getenv("WORKFLOW_ID", "f2db41e1fa331b3e")
+pretty_print_str=os.getenv("PRETTY_PRINT" , False)
+pretty_print= pretty_print_str=="True"
+indent=int(os.getenv("INDENT", 2))
+
+#
+install_tool_dependencies=True
+install_repository_dependencies=True
+
+gi = galaxy.GalaxyInstance(url=galaxy_url, key=api_key)
+
+ej = gi.workflows.export_workflow_json(workflow_id)
+
+if pretty_print:
+  print json.dumps(ej, indent=indent)
+else:
+  print json.dumps(ej)
