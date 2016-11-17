5aed5dae671960732d5c30ac069ac64f143178a1
Manabu ISHII <manabu.ishii.rb@gmail.com>
2016-11-17 08:56:38 +0000

    Update default to see stable toolshed not test

--- example/repositories_get_repository_revision_install_info.py
+++ example/repositories_get_repository_revision_install_info.py
@@ -3,7 +3,10 @@
 import os
 from bioblend import toolshed
 
-ts = toolshed.ToolShedInstance(url='https://testtoolshed.g2.bx.psu.edu')
+#
+ts = toolshed.ToolShedInstance(url='https://toolshed.g2.bx.psu.edu')
+# testtoolshed
+# ts = toolshed.ToolShedInstance(url='https://testtoolshed.g2.bx.psu.edu')
 
 rl = ts.repositories.get_repository_revision_install_info('freebayes','devteam','99684adf84de')
 
