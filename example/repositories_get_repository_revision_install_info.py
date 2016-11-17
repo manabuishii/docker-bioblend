fbe52905ba3a133479d10369ad08de66c7683fa3
Manabu ISHII <manabu.ishii.rb@gmail.com>
2016-11-17 08:53:06 +0000

    Add repositories get_repository_revision_install_info

---
+++ example/repositories_get_repository_revision_install_info.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# coding: utf-8
+import os
+from bioblend import toolshed
+
+ts = toolshed.ToolShedInstance(url='https://testtoolshed.g2.bx.psu.edu')
+
+rl = ts.repositories.get_repository_revision_install_info('freebayes','devteam','99684adf84de')
+
+print(rl)
